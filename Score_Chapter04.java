2394b6390afe0ed6babaa3c6a1cf928f866d50da
KihoGangaji <166824748+KihoGangaji@users.noreply.github.com>
2024-07-25 23:43:55 +0900

    Add files via upload

---
+++ Score_Chapter04.java
@@ -0,0 +1,20 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		System.out.println("Aさんの点数は10" + "点" );
+		System.out.println("Bさんの点数は20" + "点" );
+		System.out.println("Cさんの点数は30" + "点" );
+		System.out.println("Dさんの点数は40" + "点" );
+		System.out.println("Eさんの点数は50" + "点" );
+		System.out.println("Fさんの点数は60" + "点" );
+		System.out.println("Gさんの点数は70" + "点" );
+		System.out.println("Hさんの点数は80" + "点" );
+		System.out.println("Iさんの点数は90" + "点" );
+		System.out.println("Jさんの点数は100" + "点" );
+		System.out.println("テストの平均点は" + (550 / 10 ) + "点" );
+
+	}
+
+}
